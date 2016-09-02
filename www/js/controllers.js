4f0ffeb47a3fa39b6df42e064e178c0f141717f3
tabradford <tabradford@liberty.edu>
2016-09-02 10:46:01 -0400

    wrapped FCM plugin calls in a ready function

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,35 +28,37 @@ angular.module('starter.controllers', [])
 
   })
 
-  .controller('AppCtrl', function ($scope, $ionicModal, $timeout) {
+  .controller('AppCtrl', function ($scope, $ionicModal, $timeout, $ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      FCMPlugin.getToken(
+        function (token) {
+          alert(token);
+        },
+        function (err) {
+          console.log('error retrieving token: ' + err);
+        });
 
-    FCMPlugin.getToken(
-      function (token) {
-        alert(token);
-      },
-      function (err) {
-        console.log('error retrieving token: ' + err);
-      });
-
-    //FCMPlugin.onNotification( onNotificationCallback(data), successCallback(msg), errorCallback(err) )
-    //Here you define your application behaviour based on the notification data.
-    FCMPlugin.onNotification(
-      function (data) {
-        if (data.wasTapped) {
-          //Notification was received on device tray and tapped by the user.
-          alert(JSON.stringify(data));
-        } else {
-          //Notification was received in foreground. Maybe the user needs to be notified.
-          alert(JSON.stringify(data));
+      //FCMPlugin.onNotification( onNotificationCallback(data), successCallback(msg), errorCallback(err) )
+      //Here you define your application behaviour based on the notification data.
+      FCMPlugin.onNotification(
+        function (data) {
+          if (data.wasTapped) {
+            //Notification was received on device tray and tapped by the user.
+            alert(JSON.stringify(data));
+          } else {
+            //Notification was received in foreground. Maybe the user needs to be notified.
+            alert(JSON.stringify(data));
+          }
+        },
+        function (msg) {
+          console.log('onNotification callback successfully registered: ' + msg);
+        },
+        function (err) {
+          console.log('Error registering onNotification callback: ' + err);
         }
-      },
-      function (msg) {
-        console.log('onNotification callback successfully registered: ' + msg);
-      },
-      function (err) {
-        console.log('Error registering onNotification callback: ' + err);
-      }
-    );
+      );
+    });
+
 
     // With the new view caching in Ionic, Controllers are only called
     // when they are recreated or on app start, instead of every page change.
